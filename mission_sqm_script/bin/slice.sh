5c9a9514ca3a93e0fdfb538d42be88d314aae39d
Ignaz Möbius <9mmfullclip@gmail.com>
2016-03-04 19:57:27 +0100

    bugfixes for merger wrt/ xcam mission files

--- mission_sqm_script/bin/slice.sh
+++ mission_sqm_script/bin/slice.sh
@@ -7,9 +7,4 @@ LINES="$(grep -B99999 'class Intro' "${1}" | grep -A99999 'class Vehicles' | tai
 grep -B99999 'class Intro' "${1}" | \
     grep -A99999 'class Vehicles' | \
     tail -n '+4' | \
-    head -n "$(( ${LINES} - 3 ))" | \
-    sed 's,{,\n  {\n,g;' | \
-    sed 's,},\n  }\n,g;' | \
-    sed 's,id=,\nid=,g;' | \
-    sed 's,;,;\n,g;'
-
+    head -n "$(( ${LINES} - 3 ))"
