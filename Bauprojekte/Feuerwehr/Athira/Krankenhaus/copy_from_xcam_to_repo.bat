ccd9fbd1d5ef0413c49f77b38ab48d7fdb07b849
Ignaz Möbius <9mmfullclip@gmail.com>
2016-03-05 16:14:55 +0100

    helper script for working with xcam

---
+++ Bauprojekte/Feuerwehr/Athira/Krankenhaus/copy_from_xcam_to_repo.bat
@@ -0,0 +1,5 @@
+@echo off
+
+copy "%PROGRAMFILES(x86)%\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\_xCamProjects.ini" _xcamProjects.ini
+copy "%PROGRAMFILES(x86)%\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\Altis_KrankenhausAthira.ini" Altis_KrankenhausAthira.ini
+copy "%PROGRAMFILES(x86)%\Steam\steamapps\common\Arma 3\mission_Altis_KrankenhausAthira.sqm" mission.sqm
