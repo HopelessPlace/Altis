6f07799c2cb4e7e1f4839d4eed333872736454de
Ignaz Möbius <9mmfullclip@gmail.com>
2016-03-06 04:59:28 +0100

    Drimea, this time with the xcam.

---
+++ Bauprojekte/Rebellen/Drimea/Checkpoint/copy_from_xcam_to_repo.bat
@@ -0,0 +1,5 @@
+@echo off
+
+copy "%PROGRAMFILES(x86)%\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\_xCamProjects.ini" _xcamProjects.ini
+copy "%PROGRAMFILES(x86)%\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\Altis_CheckpointDrimea.ini" Altis_CheckpointDrimea.ini
+copy "%PROGRAMFILES(x86)%\Steam\steamapps\common\Arma 3\mission_Altis_CheckpointDrimea.sqm" mission.sqm
