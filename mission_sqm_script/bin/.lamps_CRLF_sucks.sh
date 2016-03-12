8d328a8c4cb2f6213603fef8730ee3f1a6168e47
Ignaz Möbius <9mmfullclip@gmail.com>
2016-03-12 05:17:45 +0100

    ATM and lampfix

--- mission_sqm_script/bin/.lamps_CRLF_sucks.sh
+++ mission_sqm_script/bin/.lamps_CRLF_sucks.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-LAMPS="Land_LampHarbour_F Land_LampHalogen_F Land_LampAirport_F"
+LAMPS="Land_LampHarbour_F Land_LampHalogen_F Land_LampAirport_F Land_LampDecor_F"
 
 FILE="${1}"
 
 grep -v "Land_LampHarbour_F" ${FILE} | \
     grep -v "Land_LampHalogen_F" | \
-    grep -v "Land_LampAirport_F"
+    grep -v "Land_LampAirport_F" | \
+    grep -v "Land_LampDecor_F"
 
 for LAMP in ${LAMPS}; do
     grep "${LAMP}" "${FILE}" | sed 's,this enableSimulation false;,this enableSimulation true;,g;'
